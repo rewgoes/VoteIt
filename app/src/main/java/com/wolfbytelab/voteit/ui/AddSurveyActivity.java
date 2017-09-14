e030b446d26820a7155fb978c2a5e93f1bcdf707
Rafael Wolf de Goes <rewgoes@gmail.com>
2017-09-14 11:37:27 -0300

    feat: Implement survey creation and data validation structure

--- app/src/main/java/com/wolfbytelab/voteit/ui/AddSurveyActivity.java
+++ app/src/main/java/com/wolfbytelab/voteit/ui/AddSurveyActivity.java
@@ -1,15 +1,133 @@
 package com.wolfbytelab.voteit.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.wolfbytelab.voteit.R;
+import com.wolfbytelab.voteit.model.Survey;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AddSurveyActivity extends AppCompatActivity {
+
+    @BindView(R.id.title)
+    EditText mTitle;
+    @BindView(R.id.title_input_layout)
+    TextInputLayout mTitleInputLayout;
+    @BindView(R.id.description)
+    EditText mDescription;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mSurveyDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_survey);
+        ButterKnife.bind(this);
+
+        mTitle.addTextChangedListener(new RequiredFieldTextWatcher(mTitleInputLayout));
+        mTitle.setOnFocusChangeListener(new RequiredFieldFocusChangeListener(mTitleInputLayout));
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mSurveyDatabaseReference = mFirebaseDatabase.getReference().child("surveys");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.create_menu:
+                createSurvey();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private boolean isDataValid() {
+        boolean isDataValid = true;
+
+        if (mTitle.length() == 0) {
+            mTitleInputLayout.setError(getString(R.string.fui_required_field));
+            isDataValid = false;
+        }
+
+        return isDataValid;
+    }
+
+    private void createSurvey() {
+        if (isDataValid()) {
+            Survey survey = new Survey();
+            survey.title = mTitle.getText().toString();
+            survey.description = mDescription.getText().toString();
+
+            FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+            // user is logged in
+            if (firebaseUser != null) {
+                mSurveyDatabaseReference.push().setValue(survey);
+                finish();
+            }
+        }
+    }
+
+    private class RequiredFieldTextWatcher implements TextWatcher {
+
+        TextInputLayout mTextInputLayout;
+
+        RequiredFieldTextWatcher(TextInputLayout textInputLayout) {
+            mTextInputLayout = textInputLayout;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            mTextInputLayout.setErrorEnabled(false);
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+        }
+    }
+
+    private class RequiredFieldFocusChangeListener implements View.OnFocusChangeListener {
+
+        TextInputLayout mTextInputLayout;
+
+        RequiredFieldFocusChangeListener(TextInputLayout textInputLayout) {
+            mTextInputLayout = textInputLayout;
+        }
+
+        @Override
+        public void onFocusChange(View view, boolean hasFocus) {
+            if (!hasFocus) {
+                if (view instanceof EditText) {
+                    if (((EditText) view).getText().length() == 0) {
+                        mTextInputLayout.setError(getString(R.string.fui_required_field));
+                    }
+                }
+            }
+        }
     }
 }
